345c6fe5b86f9279b5d3c5cd7c9c1f848f30218b
Eric Soyke <ESoyke@pscu.com>
2017-11-29 14:57:35 -0500

    add notes

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tour of Heroes
+
+A fork of the Angular 5 version of the tutorial project.
+
+## Issues:
+* Had to manually install the jasmine-spec-reporter library to enable 'npm run e2e'
+  `npm install jasmine-spec-reporter --savedev`
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -70,7 +70,11 @@ export class HeroService {
   /** POST: add a new hero to the server */
   addHero (hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
-      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      tap((hero: Hero) => {
+        this.log(`added hero w/ id=${hero.id}`);
+        //if(hero.name==='FOO')
+        //  throw {message: 'FOO is a lousy name for a Hero!', code: 99};
+      }),
       catchError(this.handleError<Hero>('addHero'))
     );
   }
